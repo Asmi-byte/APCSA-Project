ec9a4801ead8f6d0974791dd8e1673a07e462328
mkat2259 <168022066+mkat2259@users.noreply.github.com>
2025-01-14 08:17:54 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
+/**
+*This is the main file that we will be using to run the program
+* it takes in input using a scanner object and gives a random question to answer
+*@author Maia Katragadda & Asmita Bag & Grant Cabay & Tristian Mettee
+*/
+
+
 import java.util.*;
 
 
@@ -7,8 +14,15 @@ public class Main{
         ArrayList<Integer> usedNum = new ArrayList<Integer>();
         Scanner input = new Scanner(System.in);
         boolean wantQuestions = true;
+        int count = 0;
+        String name = "";
         
-        while(wantQuestions){
+        //Intro prompts
+        System.out.println("Hi welcome to Celeb Check, Answer our questions and will match you up with a celebrity that matches your traits. Please enter your name to start the questions: ");
+        name = input.nextLine();
+        
+        //runs until user doesn't want questions or the program runs out of questions
+        while(wantQuestions && count < 5){
 			
 			String answer = "";
 			boolean reused = true;
@@ -16,24 +30,26 @@ public class Main{
 			String stillHere = "";
 			
 			String[] questions = {"What are you passionate about? ",
-			"What adjectives would you use to describe a \"sucessful\" person? ",
-			"What adjectives would you use to describe yourself? ",
-			"What would you do if you knew you could not fail? ",
+			"How would you descirbe a \"sucessful\" person? ",
+			"How would you descibe yourself? ",
+			"What would you do if you knew you couldn’t fail? ",
 			"Describe traits that makes a good friend. ",
 			"How would you descirbe your role model? ",
-			"What type of person motivates you? ",
+			"What motivates you? ",
 			"What makes you unique? ",
 			"What are some of your interests? ",
 			"What do you think is the most important quality for a person to have in life? ",
-			"What are some personality traits you wish you had? ",
-			"How would you describe the people around you? ",
-			"What types of people do you think your morals are based on? ",
+			"What are traits do you wish you had? ",
+			"How would you descirbe the people around you? ",
+			"What do you think your morals are based on? ",
 			"How would your friends descirbe you? "};
 			
 			//Question 1
 			
+			//creates a random number with the range of the length of the list
 			int randomNum = (int)((Math.random()) * (questions.length));
 			
+			//if the random number has been used it will select another random number
 			while(reused){
 				reused = false;
 				for(int i = 0; i < usedNum.size() ; i++){
@@ -44,9 +60,9 @@ public class Main{
 				}
 			}
 			
+			//prints question stores answer in string, adds question index into used index array
 			System.out.println(questions[randomNum]);
 			answer += input.nextLine();
-			answer = answer.toLowerCase();
 			usedNum.add(randomNum);	
 			
 			//Question 2
@@ -71,7 +87,8 @@ public class Main{
 			//Keyword object 
 			
 			Keyword runner = new Keyword(answer);
-			System.out.println(runner.compareToCelebrity(runner.themeFinder()));
+			runner.themeFinder();
+			System.out.println(runner.themeFinder());
 			
 			//Asks if user wants to continue
 			
@@ -87,49 +104,16 @@ public class Main{
 					properInput = true;
 				}
 			}
-		}
-		
-
-
-
-
-	/**
-	 * proper reusing format
-	 * 
-	 
-		reused = true;
-		
-		
-		randomNum = (int)((Math.random()) * (questions.length));
-		
-		while(reused){
-			reused = false;
-			for(int i = 0; i < usedNum.size() ; i++){
-				if (randomNum == usedNum.get(i)){
-					randomNum = (int)((Math.random()) * (questions.length));
-					reused = true;
-				}
+			
+			if (count >= 4){
+				System.out.println("You're too chatty. There are no more questions to be asked.");
+				wantQuestions = false;
 			}
+			
+			count++;
+			
+
 		}
-		
-		System.out.println(questions[randomNum]);
-		
-		answer += input.nextLine();
-		
-		usedNum.add(randomNum);
-		
-		
-		System.out.println(answer);	
-		
-		**/
-		
-		
-
-		
-
-
-		
 		
 	}
 }
-	
